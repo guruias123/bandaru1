7d9e6c89b7e65efcaf8ffbb28e0eaeb51b6842a5
guruias123 <74144270+guruias123@users.noreply.github.com>
2021-03-10 16:41:15 +0530

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ const Header =()=>{
             </ul>
             
             <ul class="nav navbar-nav navbar-right">
-              <li style={{ marginTop:'0%'}}><a href="/"><p><App1/></p> </a></li>
+              <li ><App1/></li>
               <li><a href="/carts"> Cart</a></li>
             </ul>
           </div>
